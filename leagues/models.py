bd4e00257e6d2d95d8edea78257d6e80dcf24ea8
Isnard Silva <isnardsilva@icloud.com>
2020-07-25 00:10:23 -0300

    Update league field

--- leagues/models.py
+++ leagues/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from nationalities.models import Nationality
 
 # Create your models here.
 class League(models.Model):
     logo_url = models.CharField(max_length=300)
     name = models.CharField(max_length=150)
-    country = models.CharField(max_length=150)
+    country = models.ForeignKey(Nationality, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.name
