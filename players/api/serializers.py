059ae52b3fcd829ec3e81dadf216ce3dc35a1cb5
Isnard Silva <isnardsilva@icloud.com>
2020-08-01 15:42:51 -0300

    Add new fields in PlayerSerializer

--- players/api/serializers.py
+++ players/api/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from players.models import Player
 from teams.api.serializers import TeamSerializer
+from player_positions.api.serializers import PlayerPositionSerializer
 
 class PlayerSerializer(ModelSerializer):
     club = TeamSerializer()
 
+    preferred_foot = serializers.SerializerMethodField()
+    team_position = serializers.SerializerMethodField()
+
     class Meta:
         model = Player
-        fields = ['id', 'short_name', 'nationality', 'club', 'overall']
+        fields = ['id', 'short_name', 'long_name', 'date_of_birth', 'height_cm', 'weight_kg', 'nationality', 'club', 'overall', 'preferred_foot', 'team_position', 'team_jersey_number']
+
+    def get_preferred_foot(self, obj):
+        return '%s' % (obj.preferred_foot)
+
+    def get_team_position(self, obj):
+        return '%s' % (obj.team_position)
+
